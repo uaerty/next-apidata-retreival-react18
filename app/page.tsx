7e8d10ac69d39c4f5d02c18aeee94d6d8574dd88
Dr. Yatish Bathla <dr.bathla@cae.com>
2024-09-05 12:27:03 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+// ./app/page.tsx
+
+import { Suspense } from 'react';
+import Pokemon from './components/Pokemon';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Suspense
+        fallback={
+          <p className="text-orange-600 text-2xl font-bold">
+            Loading pokemon...
+          </p>
+        }
+      >
+        <Pokemon />
+      </Suspense>
+    </main>
+  );
+}
